922c6ea85a659ca77faf2d22654df46d437e1b53
Marcin Zima <mzima@novomatic-tech.com>
2023-02-10 09:36:44 +0100

    Test configuration with one test

--- src/test/java/qa/test/payrollApp/PayrollAppApplicationTests.java
+++ src/test/java/qa/test/payrollApp/PayrollAppApplicationTests.java
@@ -1,13 +1,33 @@
 package qa.test.payrollApp;
 
+import org.junit.runner.RunWith;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT, classes = PayrollAppApplicationTests.class)
+@ImportAutoConfiguration
+@ComponentScan
 class PayrollAppApplicationTests {
 
+	@Autowired
+	private WebTestClient testClient;
+
 	@Test
-	void contextLoads() {
+	void getAllCompanies() {
+		testClient
+				.get()
+				.uri("/companies")
+				.exchange()
+				.expectStatus().isOk()
+				.expectBody()
+				.jsonPath("$[0].name").isEqualTo("Apple")
+				.jsonPath("$[1].name").isEqualTo("Amazon");
 	}
 
 }
---
+++ src/test/java/qa/test/payrollApp/controller/configuration/TestsConfiguration.java
@@ -0,0 +1,17 @@
+package qa.test.payrollApp.controller.configuration;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@TestConfiguration
+public class TestsConfiguration {
+
+    @Bean
+    public WebTestClient webTestClient(){
+        return WebTestClient
+                .bindToServer()
+                .baseUrl("http://localhost:8080")
+                .build();
+    }
+}
